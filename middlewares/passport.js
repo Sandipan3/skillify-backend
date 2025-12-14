3adfe2ffcc0d5b73fa5d761a7776bd9ae073d457
SANDIPAN JHA <sandipanjha3@gmail.com>
2025-12-14 11:21:52 +0530

    Fix: social login fixed

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,13 +1,15 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../models/User.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/v1/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
