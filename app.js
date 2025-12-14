e81da219f35ffc714110f3c267038144e68251f0
SANDIPAN JHA <sandipanjha3@gmail.com>
2025-12-14 11:28:03 +0530

    Fix: cors origin changed

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//   })
-// );
-//====localhost=====================
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
+//====localhost=====================
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 //=======================================
 
 // MongoDB Connection
