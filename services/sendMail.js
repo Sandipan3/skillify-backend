dbd4f7232fa6bae41fa492e6062580ea503d0473
SANDIPAN JHA <sandipanjha3@gmail.com>
2025-12-20 20:59:28 +0530

    Fix : switched to brevo for mail

--- services/sendMail.js
+++ services/sendMail.js
@@ -1,19 +1,41 @@
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_KEY);
+// import axios from "axios";
 
 export const verifyRegisterEmail = async (userEmail, otp) => {
   try {
-    const res = await resend.emails.send({
-      from: "Task Management <no-reply@resend.dev>",
-      to: userEmail,
-      subject: "Verify your email",
-      html: `<p>Your OTP is <b>${otp}</b></p>`,
-    });
+    const res = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "Task Management",
+          email: "no-reply@skillify.dev",
+        },
+        to: [
+          {
+            email: userEmail,
+          },
+        ],
+        subject: "Verify your email",
+        htmlContent: `
+          <div style="font-family: Arial, sans-serif">
+            <h3>Email Verification</h3>
+            <p>Your OTP is:</p>
+            <h2>${otp}</h2>
+            <p>This OTP is valid for 15 minutes.</p>
+          </div>
+        `,
+      },
+      {
+        headers: {
+          "api-key": process.env.BREVO_KEY,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
 
-    console.log("RESEND RESPONSE:", res);
+    console.log("BREVO RESPONSE:", res.data);
   } catch (error) {
-    console.error("RESEND ERROR:", error);
+    console.error("BREVO ERROR:", error.response?.data || error.message);
     throw error;
   }
 };
