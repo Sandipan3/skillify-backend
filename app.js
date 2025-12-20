621ae2f624629710ff87893a2d51835cefe17cde
SANDIPAN JHA <sandipanjha3@gmail.com>
2025-12-20 22:10:52 +0530

    Fix : minor changes

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import morgan from "morgan";
 import cors from "cors";
 import courseRoutes from "./routes/courseRoutes.js";
