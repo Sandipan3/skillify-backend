2d234be1652e9713a21f5c5aed276d964e15e64a
SANDIPAN JHA <sandipanjha3@gmail.com>
2026-01-11 20:57:43 +0530

    Fix : minor changes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,4 @@
-import User from "../models/User";
+import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 import { sendErrorResponse, sendSuccessResponse } from "../utils/response";
 import { sendMail } from "../services/sendMail.js";
